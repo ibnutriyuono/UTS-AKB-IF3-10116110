aaac1ced2b5b5a1b94c18ccbb2a9a44cfbd0287c
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-05-20 00:20:44 +0700

    adding layout for data teman fragment

--- app/src/main/java/com/example/k43sj/uts_akb_if3_10116110/DataTemanFragment.java
+++ app/src/main/java/com/example/k43sj/uts_akb_if3_10116110/DataTemanFragment.java
@@ -1,4 +1,29 @@
 package com.example.k43sj.uts_akb_if3_10116110;
 
-public class DataTemanFragment {
+/*
+ * 19 Mei 2019
+ * 10116110
+ * Muhamad Ibnu Tri Yuono
+ * IF-3
+ */
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DataTemanFragment extends Fragment {
+    public DataTemanFragment(){
+    }
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.data_teman_layout, container, false);
+    }
 }
