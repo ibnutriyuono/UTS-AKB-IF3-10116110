5940c88037d65d851ee3b35baa7f1657bc526373
ibnutriyuono <ibnutriyuono23@gmail.com>
2019-05-20 06:19:21 +0700

    mahasiswa model

--- app/src/main/java/com/example/k43sj/uts_akb_if3_10116110/DataTemanActivity.java
+++ app/src/main/java/com/example/k43sj/uts_akb_if3_10116110/DataTemanActivity.java
@@ -1,6 +1,7 @@
 package com.example.k43sj.uts_akb_if3_10116110;
+
 /*
- * 19 Mei 2019
+ * 20 Mei 2019
  * 10116110
  * Muhamad Ibnu Tri Yuono
  * IF-3
@@ -8,9 +9,6 @@ package com.example.k43sj.uts_akb_if3_10116110;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -20,25 +18,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.k43sj.uts_akb_if3_10116110.fragment.DataTemanFragment;
-import com.example.k43sj.uts_akb_if3_10116110.fragment.KontakFragment;
-
-public class MainActivity extends AppCompatActivity
+public class DataTemanActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_data_teman);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        DataDiriFragment dataDiriFragment = new DataDiriFragment();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.frame_container, dataDiriFragment);
-        getSupportActionBar().setTitle("Data Diri");
-        fragmentTransaction.commit();
-
+        getSupportActionBar().setTitle("Data Teman");
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -61,39 +50,47 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main2, menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.data_teman, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
-        Fragment fragment = null;
-        if (id == R.id.nav_person) {
-            fragment = new DataDiriFragment();
-            getSupportActionBar().setTitle("Data Diri");
-        } else if (id == R.id.nav_contacts) {
-            fragment= new KontakFragment();
-            getSupportActionBar().setTitle("Kontak Diri");
-        } else if (id == R.id.nav_people) {
-            Intent intent = new Intent(MainActivity.this, DataTemanActivity.class);
+
+        if (id == R.id.nav_back) {
+            Intent intent = new Intent(DataTemanActivity.this, MainActivity.class);
             startActivity(intent);
         }
-        if (fragment != null) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
-            fragmentManager.beginTransaction()
-                    .replace(R.id.frame_container, fragment).commit();
-        }
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
---
+++ app/src/main/java/com/example/k43sj/uts_akb_if3_10116110/model/Mahasiswa.java
@@ -0,0 +1,70 @@
+package com.example.k43sj.uts_akb_if3_10116110.model;
+
+/*
+ * 20 Mei 2019
+ * 10116110
+ * Muhamad Ibnu Tri Yuono
+ * IF-3
+ */
+
+public class Mahasiswa {
+    private String nim,nama,kelas,telephone,email,instagram;
+
+    public Mahasiswa(String nim, String nama, String kelas, String telephone, String email, String instagram){
+        this.nim = nim;
+        this.nama = nama;
+        this.kelas = kelas;
+        this.telephone = telephone;
+        this.email = email;
+        this.instagram = instagram;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getInstagram() {
+        return instagram;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+
+    public void setInstagram(String instagram) {
+        this.instagram = instagram;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+}
